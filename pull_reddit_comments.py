6d23e96bc51393111b4b46ab7e2354e0f5d6b06d
Ajax12345 <jpetullo14@gmail.com>
2024-01-17 15:44:13 -0500

    comments scrapped

--- pull_reddit_comments.py
+++ pull_reddit_comments.py
@@ -1,8 +1,27 @@
 import typing, requests, json
-import praw, os
+import praw, os, urllib.parse
+from praw.models import MoreComments
+from collections import deque
 
-print(os.getcwd())
 
+BASE_URL = 'https://www.reddit.com/r/russian/'
 
 reddit = praw.Reddit('NLPTesting')
+
+subreddit = reddit.subreddit('russian')
+
+all_comments = []
+
+
+
+for submission in subreddit.top(limit = 10):
+    submission.comments.replace_more(limit=None)
+    queue, results = deque(submission.comments.list()), []
+    while queue:
+        comment = queue.popleft()
+        results.append({'permalink': comment.permalink, 'body': comment.body})
+        queue.extend(comment.replies)
+
+    with open('r_russian_reddit_comments.jsonl', 'a') as f:
+        f.write('\n'.join(map(json.dumps, results)))
 
